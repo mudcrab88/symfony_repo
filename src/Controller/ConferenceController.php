3406e14d1d8b847c10cbdbad71270254ff45aaab
Nickolay Kuznetsov <mudcrab@yandex.ru>
2021-08-01 00:01:39 +0400

    add entities, fix routes

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -8,11 +8,24 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
+	/**
+     * @Route("/", name="homepage")
+     */
+    public function index(): Response
+    {
+		return new Response('<html><body>Main Page</body></html>');
+    }
+	
     /**
      * @Route("/conference", name="conference")
      */
-    public function index(): Response
-    {        
-		return new Response('<html><body>PPC</body></html>');
+    public function conference(): Response
+    {
+		return new Response('<html><body>Conference</body></html>');
+    }
+	
+	public function test(): Response
+    {
+		return new Response('<html><body>Test</body></html>');
     }
 }
