30c5aefe674d75c6f2c762a5b1b6a169f058e592
mudcrab88 <mudcrab@yandex.ru>
2021-08-16 00:02:49 +0400

    add twig views

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,30 +2,48 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Entity\Conference;
+use App\Repository\ConferenceRepository;
+use App\Repository\CommentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Twig\Environment;
 
 class ConferenceController extends AbstractController
 {
-	/**
-     * @Route("/", name="homepage")
-     */
-    public function index(): Response
+    private $twig;
+
+    public function __construct(Environment $twig)
     {
-		return new Response('<html><body>Main Page</body></html>');
+        $this->twig = $twig;
     }
-	
     /**
-     * @Route("/conference", name="conference")
+     * @Route("/", name="homepage")
+     * @param Environment          $twig
+     * @param ConferenceRepository $conferenceRepository
+     *
+     * @return Response
      */
-    public function conference(): Response
+    public function index(ConferenceRepository $conferenceRepository): Response
     {
-		return new Response('<html><body>Conference</body></html>');
+        return new Response($this->twig->render('conference/index.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
     }
-	
-	public function test(): Response
+
+    public function test(): Response
     {
-		return new Response('<html><body>Test</body></html>');
+        return new Response('<html><body>Test</body></html>');
+    }
+
+    #[Route('/conference/{id}', name: 'conference')]
+    public function show(Conference $conference, CommentRepository $commentRepository): Response
+    {
+        return new Response($this->twig->render('conference/show.html.twig', [
+            'conference' => $conference,
+            'comments' => $commentRepository->findBy(['conference' => $conference], ['createdAt' => 'DESC']),
+        ]));
     }
 }
