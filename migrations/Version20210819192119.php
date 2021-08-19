b539aa45e74aeceeb851614ffc9dbc2670600f52
mudcrab88 <mudcrab@yandex.ru>
2021-08-19 23:28:54 +0400

    add slug to conference

--- migrations/Version20210819192119.php
+++ migrations/Version20210819192119.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210817183919 extends AbstractMigration
+final class Version20210819192119 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,19 +20,15 @@ final class Version20210817183919 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('
-            CREATE TABLE sessions (
-                sess_id VARCHAR(128) NOT NULL PRIMARY KEY,
-                sess_data BYTEA NOT NULL,
-                sess_lifetime INTEGER NOT NULL,
-                sess_time INTEGER NOT NULL
-           )
-        ');
+        $this->addSql('ALTER TABLE conference ADD slug VARCHAR(255)');
+        $this->addSql("UPDATE conference SET slug=CONCAT(LOWER(city), '-', year)");
+        $this->addSql('ALTER TABLE conference ALTER COLUMN slug SET NOT NULL');
+
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE sessions');
+        $this->addSql('ALTER TABLE conference DROP COLUMN slug');
     }
 }
