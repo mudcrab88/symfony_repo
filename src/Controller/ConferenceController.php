d45acb3f1259eecd0c64dec52da5d5ec46a412c3
mudcrab88 <mudcrab@yandex.ru>
2021-08-23 00:15:35 +0400

    add slug urls

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -48,7 +48,7 @@ class ConferenceController extends AbstractController
         return new Response('<html><body>Test</body></html>');
     }
 
-    #[Route('/conference/{id}', name: 'conference')]
+    #[Route('/conference/{slug}', name: 'conference')]
     public function show(Request $request, Conference $conference): Response
     {
         $offset = max(0, $request->query->getInt('offset', 0));
