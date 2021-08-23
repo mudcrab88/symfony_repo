64c456c17ea15657493617c5150b2866e80ae0ac
mudcrab88 <mudcrab@yandex.ru>
2021-08-23 23:46:15 +0400

    add comment form

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Comment;
 use App\Entity\Conference;
-use App\Repository\ConferenceRepository;
+use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
+use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -20,15 +22,20 @@ class ConferenceController extends AbstractController
 
     private $conferenceRepository;
 
+    private $entityManager;
+
     public function __construct(
         Environment $twig,
         CommentRepository $commentRepository,
-        ConferenceRepository $conferenceRepository)
-    {
+        ConferenceRepository $conferenceRepository,
+        EntityManagerInterface $entityManager
+    ) {
         $this->twig = $twig;
         $this->commentRepository = $commentRepository;
         $this->conferenceRepository = $conferenceRepository;
+        $this->entityManager = $entityManager;
     }
+
     /**
      * @Route("/", name="homepage")
      * @param Environment          $twig
@@ -51,14 +58,27 @@ class ConferenceController extends AbstractController
     #[Route('/conference/{slug}', name: 'conference')]
     public function show(Request $request, Conference $conference): Response
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setConference($conference);
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $this->commentRepository->getCommentPaginator($conference, $offset);
 
         return new Response($this->twig->render('conference/show.html.twig', [
-            'conference' => $conference,
-            'comments' => $paginator,
-            'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
-            'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+                'conference'   => $conference,
+                'comments'     => $paginator,
+                'previous'     => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+                'next'         => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+                'comment_form' => $form->createView(),
             ])
         );
     }
