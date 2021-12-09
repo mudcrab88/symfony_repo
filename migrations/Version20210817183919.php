3f0d614b9c4e12a1f10154fc188b55d67bc5b025
mudcrab88 <mudcrab@yandex.ru>
2021-12-09 17:40:10 +0400

    delete admin

--- migrations/Version20210817183919.php
+++ migrations/Version20210817183919.php
@@ -20,9 +20,6 @@ final class Version20210817183919 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE admin_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE admin (id INT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE UNIQUE INDEX UNIQ_880E0D76F85E0677 ON admin (username)');
         $this->addSql('
             CREATE TABLE sessions (
                 sess_id VARCHAR(128) NOT NULL PRIMARY KEY,
@@ -36,9 +33,6 @@ final class Version20210817183919 extends AbstractMigration
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE admin_id_seq CASCADE');
-        $this->addSql('DROP TABLE admin');
         $this->addSql('DROP TABLE sessions');
     }
 }
